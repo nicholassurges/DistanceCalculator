ee527043df66f2246e86cff5c2be778be51b8b75
nicholassurges <81642890+nicholassurges@users.noreply.github.com>
2022-03-31 21:32:17 -0400

    Add files via upload

---
+++ Distance Calculator/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstDistance = New System.Windows.Forms.ListBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstDistance
+        '
+        Me.lstDistance.FormattingEnabled = True
+        Me.lstDistance.ItemHeight = 16
+        Me.lstDistance.Location = New System.Drawing.Point(12, 12)
+        Me.lstDistance.Name = "lstDistance"
+        Me.lstDistance.Size = New System.Drawing.Size(358, 228)
+        Me.lstDistance.TabIndex = 0
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(205, 266)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(96, 44)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalc.Location = New System.Drawing.Point(73, 266)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(96, 44)
+        Me.btnCalc.TabIndex = 2
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(382, 353)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lstDistance)
+        Me.Name = "Form1"
+        Me.Text = "Distance Calculator"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstDistance As ListBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnCalc As Button
+End Class
---
+++ Distance Calculator/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the program'
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim strInputSpeed As String 'Input Speed'
+        Dim strInputTime As String 'Input Time'
+        Dim intTotal As Integer = 0 'Loop Counter'
+        Dim intDistance As Integer = 0 'Distance'
+        Dim decSpeed As Decimal = 0 'Speed'
+        Dim decTime As Decimal = 0 'Time Driving'
+        Dim decTotal As Decimal = 0 'Total Distance'
+
+        'Input Speed'
+        strInputSpeed = InputBox("Enter the speed of the vehicle in miles-per-hour")
+
+        'Convert to decimal'
+        If Decimal.TryParse(strInputSpeed, decSpeed) Then
+
+            'Get Time'
+            strInputTime = InputBox("Enter the time in hours that the vehicle is traveling")
+
+            'Convert to Decimal"
+            If Decimal.TryParse(strInputTime, decTime) Then
+
+                'Calculate Time'
+                decTotal = decTime * decSpeed
+
+                'Display Speed'
+                strInputSpeed = "Vehicle Speed: " & decSpeed.ToString() & " MPH"
+                lstDistance.Items.Add(strInputSpeed)
+
+                'Display Time Traveled'
+                lstDistance.Items.Add("Time Traveled: " & decTime.ToString() & " Hours")
+
+                'Adds Hours and Distance Columns"
+                lstDistance.Items.Add("Hours      Distance Traveled")
+
+                'Show Gap Seperation'
+                lstDistance.Items.Add("_________________________")
+
+                For intTotal = 1 To 7
+                    'Calculate Distance'
+                    intDistance = CInt(intTotal * decSpeed)
+                    lstDistance.Items.Add(intTotal.ToString & "               " & intDistance.ToString)
+
+                Next
+
+                lstDistance.Items.Add("Total Distance: " & decTotal.ToString)
+            Else
+                MessageBox.Show("Error: Enter Numberic Value For Time.")
+            End If
+        Else
+            MessageBox.Show("Error: Enter Numeric Value For Speed.")
+        End If
+
+    End Sub
+End Class
